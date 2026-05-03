5da454177ed989a6667144983063726dc1029beb
Leo-Mialaret <leo.mialaret.pro@gmail.com>
2026-05-03 12:32:26 +0200

    config change to add site and base

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
-
 import icon from 'astro-icon';
 
-// https://astro.build/config
 export default defineConfig({
-  // site: '#',
-  // base: '/',
+  site: 'https://leo-mialaret.github.io',
+  base: '/Portfolio_site',
   vite: {
     plugins: [tailwindcss()]
   },
-
   integrations: [icon()]
 });
